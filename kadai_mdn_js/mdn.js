450e887b33e31761f444d82ca1a61de4c05b44bf
kurodakeita <167636389+kurodakeita@users.noreply.github.com>
2024-06-11 22:01:32 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,7 @@
+const today = new Date();
+const year = today.getFullYear();
+const month = today.getUTCMonth();
+const date = today.getDate();
+
+console.log (year + '年', month+1 + '月', date +'日');
+
