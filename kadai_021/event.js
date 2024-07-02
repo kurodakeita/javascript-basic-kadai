401e95eab7ef4bec138b189fe1f4dd2ed9712804
kurodakeita <167636389+kurodakeita@users.noreply.github.com>
2024-07-02 21:00:41 +0900

    Add files via upload

---
+++ kadai_021/event.js
@@ -0,0 +1,8 @@
+const botn= document. getElementById('btn')
+
+botn,addEventListener('click',()=>{
+  const teXt= document. getElementById('text');
+  setTimeout(() => {
+    teXt.textContent ='ボタンをクリックしました。';
+  }, 2000);
+});
