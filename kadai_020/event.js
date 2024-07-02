e88bb595fbc6937c5a22750add054f54b9e5e663
kurodakeita <167636389+kurodakeita@users.noreply.github.com>
2024-07-02 21:12:39 +0900

    Delete kadai_020 directory

--- kadai_020/event.js
+++
@@ -1,7 +0,0 @@
-const botn= document. getElementById('btn')
-
-botn,addEventListener('click',()=>{
-  const teXt= document. getElementById('text');
-  teXt.textContent = 'ボタンをクリックしました。';
-  console.log(teXt);
-});
